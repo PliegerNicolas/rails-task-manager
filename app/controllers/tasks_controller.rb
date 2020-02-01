bac1d17bac89e5824594bbbaf08034e7a3058749
Nicolas Plieger <plieger05@gmail.com>
2020-02-01 10:29:25 +0000

    Making it functionnal

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %I[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    id = params[:id]
-    @task = Task.find(id)
   end
 
   def new
@@ -13,27 +13,61 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task_created = Task.create(task_params) unless params[:task][:title] == ''
-    redirect_to tasks_path
+    Task.create(task_params)
   end
 
-  def delete
-    id = params[:id]
-    @task_deleted = Task.delete(id)
-    redirect_to tasks_path
+  def edit
   end
 
   def update
-    id = params[:id]
-    @task_update = Task.find(id)
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
-  def update_action
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details)
   end
 end
+
+# def index
+#    @tasks = Task.all
+#  end
+
+#  def show
+#    id = params[:id]
+#    @task = Task.find(id)
+#  end
+
+#  def new
+#    @task = Task.new
+#  end
+
+#  def create
+#    @task_created = Task.create(task_params) unless params[:task][:title] == ''
+#    redirect_to tasks_path
+#  end
+
+#  def delete
+#    id = params[:id]
+#    @task_deleted = Task.delete(id)
+#    redirect_to tasks_path
+#  end
+
+#  def edit
+#    id = params[:id]
+#    @task_edit = Task.find(id)
+#  end
+
+#  def update
+#  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
   get 'tasks', to: 'tasks#index'
   post 'tasks', to: 'tasks#create'
-  delete 'tasks/:id', to: 'tasks#delete', as: 'delete'
-  get 'show/edit/:id', to: 'tasks#update', as: 'update'
-  patch 'tasks', to: 'tasks#update_action', as: 'update_action'
-  get 'show/:id', to: 'tasks#show', as: 'show'
-  get 'new', to: 'tasks#new'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
   root 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+#  get 'tasks', to: 'tasks#index'
+#  post 'tasks', to: 'tasks#create'
+#  delete 'tasks/:id', to: 'tasks#delete', as: 'delete'
+#  get 'show/edit/:id', to: 'tasks#edit', as: 'edit'
+#  patch 'tasks', to: 'tasks#update', as: 'update'
+#  get 'show/:id', to: 'tasks#show', as: 'show'
+#  get 'new', to: 'tasks#new'
+#  root 'tasks#index'
